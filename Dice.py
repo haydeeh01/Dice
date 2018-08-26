fe7997a1e1c773cdcc0d45d88f8ee0f8e6ebd8a4
haydeeh01 <42624576+haydeeh01@users.noreply.github.com>
2018-08-26 12:31:05 -0400

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+min = 1
+max = 6
+done = False
+question = input("Type R to roll the dice")
+if question.lower() == "r":
+    print("Rolling...")
+    time.sleep(1)
+    print(random.randint(min, max))
+    print(random.randint(min, max))
+while not done:
+    again =  input("Would you like to roll again? Y/N")
+    if again.lower() == "n":
+        print("Goodbye.")
+        time.sleep(1)
+        done = True
+        exit()
+    if again.lower() =="y":
+        done =  False
+        print("Rolling...")
+        time.sleep(1)
+        print(random.randint(min, max))
+        print(random.randint(min, max))
